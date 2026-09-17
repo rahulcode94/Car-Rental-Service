1de43906d7c42396c7c590dd5bf1a91f5a12bf79
Rahul <rahulkumargupta9326@gmail.com>
2026-09-17 21:51:59 +0530

    understanding jwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,28 +1,88 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 
-export const registerUser = async (req,res) => {
+
+//generate jwt token
+const generateToken = (userId) => {
+    const payload = userId
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
+//Register user
+export const registerUser = async (req, res) => {
     try {
-        const {name, email, password} = req.body
+        const { name, email, password } = req.body
 
-        if(!name || !email || !password  || password.length < 8){
-            return res.json({success:false, massage:'fill all the fields'})
+        if (!name || !email || !password || password.length < 8) {
+            return res.json({ success: false, massage: 'fill all the fields' })
 
         }
 
-        const userExists = await User.findOne({email})
-        if(userExists){
-            return res.json({success:false, message:'User already exists'})
+        const userExists = await User.findOne({ email })
+        if (userExists) {
+            return res.json({ success: false, message: 'User already exists' })
         }
 
         //it will hash password of user and the create user
         const hashedPassword = await bcrypt.hash(password, 10)
-        const user = await User.create({name,email,password:hashedPassword})
+        const user = await User.create({ name, email, password: hashedPassword })
 
-        
+        //it will convert token to string 
+        const token = generateToken(user._id.toString())
+        res.json({ success: true , token:token})
+
     } catch (error) {
-        
+        console.log(error.massage);
+        res.json({ success: false, massage: error.massage })
+
+    }
+
+
+}
+
+//login user
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        //it will find user in database
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, massage: 'User not found' })
+        }
+
+        //it will match the password of that user
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.json({ success: false, massage: 'Invalid Credentials' })
+        }
+
+        const token = generateToken(user._id.toString())
+        res.json({ success: true,  token })
+
+    } catch (error) {
+        console.log(error.massage);
+        res.json({ success: false, massage: error.massage })
+    }
+
+}
+
+
+
+
+//Get user data using Token (JWT)
+export const getUserData = async (req,res) => {
+    try {
+        const {user} = req;
+        res.json({success:true, user})
+    } catch (error) {
+         console.log(error.massage);
+        res.json({ success: false, massage: error.massage })
     }
     
 }
+
+
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 
@@ -18,6 +19,11 @@ app.use(express.json())
 
 //route
 app.get("/",(req,res)=>res.send("Server is running"))
+
+//user Router
+app.use('/api/user',userRouter)
+
+
 
 //port
 const PORT = process.env.PORT || 3000
